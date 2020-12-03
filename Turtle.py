6371aa76157bbdfa16ce1bab888c45361b5cfa7c
kev1e <57797330+kev1e@users.noreply.github.com>
2020-12-03 12:47:46 +0300

    1
    1

--- Turtle.py
+++ Turtle.py
@@ -1,20 +1,50 @@
+def find_the_way(graph, start, goal):  # start(x,y) goal(x,y)
+    # finding the way out
+    explored = []
+    queue = [[start]]
+    if start == goal:
+        return []
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+        if node not in explored:
+            neighbours = graph[node]
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+                if neighbour == goal:
+                    return new_path
+            explored.append(node)
+    return None
+
+
 class LabirintTurtle():
     def __init__(self):
         self.file = None
         self.correct_map = []
         self.matrix_game = []
         self.koordinats = []
-        self.is_map_valid = True
-        self.check_map = True
         self.graph = None
+        self.x = None
+        self.y = None
+        self.exits = []
+        self.paths = []
 
     def load_map(self, name):
         self.file = open(name)
         s = self.file.read().split('\n')
         for i in range(len(s) - 2):
             self.matrix_game.append(list(s[i]))
-        self.koordinats.append(int(s[-2]))
-        self.koordinats.append(int(s[-1]))
+        try:
+            self.x = self.koordinats.append(int(s[-2]))
+            self.y = self.koordinats.append(int(s[-1]))
+            if self.check_map() == "Ошибка":
+                print("Карта не валидна")
+                self.load_map(input())
+        except ValueError:
+            print("Координат черепахи нет")
+            self.load_map(input())
         self.file.close()
 
     def show_map(self, turtle=False):
@@ -26,13 +56,43 @@ class LabirintTurtle():
             print(*i, sep='\t')
 
     def check_map(self):
+        for i in range(len(self.matrix_game)):
+            for j in range(len(self.matrix_game[i])):
+                if self.matrix_game[i][j] != ' ' and self.matrix_game[i][j] != '*':
+                    return "Ошибка"
+        if self.matrix_game[self.koordinats[0]][self.koordinats[1]] == '*':
+            return "Ошибка"
+        for i in range(len(self.matrix_game)):
+            for j in range(len(self.matrix_game[i])):
+                if i == 0 or j == 0 or i == len(self.matrix_game) - 1 or i == len(self.matrix_game[0]) - 1:
+                    if self.matrix_game[i][j] == ' ':
+                        self.exits.append([i, j])
+        if self.exits == []:
+            return "Ошибка"
+        start = str(self.koordinats[0]) + ',' + str(self.koordinats[1])
+        for exit in self.exits:
+            exit_str = str(exit[0]) + ',' + str(exit[1])
+            result = find_the_way(self.making_graph(), start, exit_str)
+            if result != None:
+                self.paths.append(result)
+        if self.paths == []:
+            return "Ошибка"
+        return '.'
         
 
     def exit_count_step(self):
-        pass
+        print(len(self.paths[0]))
 
     def exit_show_step(self):
-        pass
+        for i in range(len(self.paths[0])):
+            x = int(self.paths[0][i][0])
+            y = int(self.paths[0][i][2])
+            self.matrix_game[x][y] = '.'
+        self.show_map()
+        for i in range(len(self.paths[0])):
+            x = int(self.paths[0][i][0])
+            y = int(self.paths[0][i][2])
+            self.matrix_game[x][y] = ' '
 
     def making_graph(self):
         graph = dict()
@@ -70,28 +130,11 @@ class LabirintTurtle():
                             graph[a] = mass
         return graph
 
-    def find_the_way(graph, start, goal): # start(x,y) goal(x,y)
-        # finding the way out
-        explored = []
-        queue = [[start]]
-        if start == goal:
-            return []
-        while queue:
-            path = queue.pop(0)
-            node = path[-1]
-            if node not in explored:
-                neighbours = graph[node]
-                for neighbour in neighbours:
-                    new_path = list(path)
-                    new_path.append(neighbour)
-                    queue.append(new_path)
-                    if neighbour == goal:
-                        return new_path
-                explored.append(node)
-        return None
 
 
-turtle = LabirintTurtle()
-turtle.load_map("Table.txt")
-turtle.show_map(turtle=True)
-turtle.check_map()
+
+#turtle = LabirintTurtle()
+#turtle.load_map("Table.txt")
+#turtle.show_map(turtle=True)
+#print('---------------------')
+#turtle.exit_show_step()
